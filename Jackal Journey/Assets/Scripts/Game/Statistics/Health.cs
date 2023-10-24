6777ae4f7d15e3dbd42f0d2845991195e85c4dc9
mszczechowicz <scela0718@gmail.com>
2023-10-24 20:35:36 +0200

    ADDON: HeathBar UI and Logic'

--- Jackal Journey/Assets/Scripts/Game/Statistics/Health.cs
+++ Jackal Journey/Assets/Scripts/Game/Statistics/Health.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private int maxHealth = 1000;
+    [SerializeField] private Slider true_healthSlider;
+    [SerializeField] private Slider easy_healthSlider;
+    private float easylerpingintoHeath = 0.01f;
 
     private int health;
     private bool isInvulnerable;
@@ -14,12 +19,31 @@ public class Health : MonoBehaviour
     
     public event Action OnDie;
 
+    
+    private void Update()
+    {
+        //do przeniesienia te wartoœci tam gdzie bedzie zadawane obra¿enie a nie w updacie wszystko na si³ê robione
+        true_healthSlider.value = health;
+        
 
+        if (true_healthSlider.value != easy_healthSlider.value)
+        {
+            easy_healthSlider.value = Mathf.Lerp(easy_healthSlider.value, true_healthSlider.value, easylerpingintoHeath);
+            Debug.Log("EASYSLIDERONONON");
+        }
+    }
 
     public bool IsDead => health == 0;
     private void Start()
     {
+        
+
         health = maxHealth;
+        true_healthSlider.maxValue = maxHealth;
+        true_healthSlider.minValue = 0;
+
+        easy_healthSlider.maxValue = maxHealth;
+        easy_healthSlider.minValue = 0;
     }
 
     public void SetInvulnerable(bool isInvulnerable)
