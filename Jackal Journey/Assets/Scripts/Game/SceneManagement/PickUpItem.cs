1d3eee1b6b917f5c44cd8e3bfcff4b4d5724f872
mszczechowicz <scela0718@gmail.com>
2023-11-26 13:58:21 +0100

    Addon:Implement CollectData window and unity analytics;

--- Jackal Journey/Assets/Scripts/Game/SceneManagement/PickUpItem.cs
+++ Jackal Journey/Assets/Scripts/Game/SceneManagement/PickUpItem.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.Services.Analytics;
 using UnityEngine;
+#if ENABLE_CLOUD_SERVICES_ANALYTICS
+using UnityEngine.Analytics;
+#endif
 
 public class PickUpItem : MonoBehaviour, IInteractable
 {
-    [SerializeField] private GameObject savemanager;
+    
     [field: SerializeField] public GameObject Player { get; set; }
 
 
     public void Interact()
     {
-        //savemanager.GetComponent<SaveManager>().LoadData();
+        int totalPotions = 5;
+        int totalCoins = 100;
+#if ENABLE_CLOUD_SERVICES_ANALYTICS
+        Debug.Log("GamveOverEventAnalitics");
+        Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+        {
+            { "potions", totalPotions },
+            { "coins", totalCoins }
+        });
+        AnalyticsService.Instance.CustomData("gameOver");
+#endif
         Player.GetComponent<InteractionHandler>().IsInteracting = false;
     }
 
