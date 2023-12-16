a5288c3b63c786dd4faadb4ebbea5437dbc0615b
mszczechowicz <scela0718@gmail.com>
2023-12-16 10:42:31 +0100

    Addon: HIntsCanvas; Rebuild GivingDataConsent in MainMenu;

---
+++ Jackal Journey/Assets/HintsShowUp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.UI;
+
+public class HintsShowUp : MonoBehaviour
+{
+    [SerializeField] GameObject HintsImage;
+
+    private const string HintsShowed = " HintsShowed";
+    
+
+    private void Start()
+    {
+        bool ShowNextTime = PlayerPrefs.GetInt(HintsShowed, 0) == 1;
+
+        if (ShowNextTime)
+        {
+            HintsImage.SetActive(false);
+
+        }
+        else
+        {
+            StartCoroutine(CloseHint());
+        }
+    }
+
+    private IEnumerator CloseHint()
+    {
+        yield return new WaitForSeconds(5);
+        HintsImage?.SetActive(false);
+        PlayerPrefs.SetInt(HintsShowed, 1);
+
+    }
+}
