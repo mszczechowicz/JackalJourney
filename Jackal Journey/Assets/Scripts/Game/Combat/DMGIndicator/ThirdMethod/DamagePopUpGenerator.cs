12ec45f823bed59e584d404cdfc25ef6f942cdf2
Rix34 <r.skoczek34@gmail.com>
2023-12-21 03:12:57 +0100

    Second try
    Creating second type of popupgenerator

---
+++ Jackal Journey/Assets/Scripts/Game/Combat/DMGIndicator/ThirdMethod/DamagePopUpGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopUpGenerator : MonoBehaviour
+{
+    public static DamagePopUpGenerator current;
+    public GameObject prefab;
+    public Transform place;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        current = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) { 
+        
+            CreatePopUp(Vector3.one,Random.Range(0,1000).ToString(), Color.red);
+
+        }
+        
+    }
+
+    public void CreatePopUp(Vector3 position, string text, Color color)
+    {
+        var popup = Instantiate(prefab, place.transform.position, Quaternion.identity);
+        var temp  = popup.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        temp.text = text;
+        temp.faceColor= color;
+        Destroy(popup, 1f);
+    }
+
+}
+
