14ea08d4437939035d375acfd1f6b8a24b888a91
Rix34 <r.skoczek34@gmail.com>
2024-01-19 18:00:11 +0100

    Test Sound

--- Jackal Journey/Assets/Art/Shaders/WeaponEffect/SwordSlash/SlashScript/SlashEnable.cs
+++ Jackal Journey/Assets/Art/Shaders/WeaponEffect/SwordSlash/SlashScript/SlashEnable.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.VFX;
 
 [Serializable]
@@ -10,19 +11,23 @@ public class VFXHandler : MonoBehaviour
 
     [SerializeField] private VisualEffect SlashVFX1;
     [SerializeField] private VisualEffect SlashVFX2;
-    [SerializeField] private VisualEffect SlashVFX3;    
+    [SerializeField] private VisualEffect SlashVFX3;
+    [SerializeField] private UnityEvent SlashSound;
 
     public void PlayVFX1()
-    {       
+    {
+        SlashSound.Invoke();
         SlashVFX1.Play();
     }
     public void PlayVFX2()
     {
+        SlashSound.Invoke();
         SlashVFX2.Play();
 
     }
     public void PlayVFX3()
     {
+        SlashSound.Invoke();
         SlashVFX3.Play();
     }
 }
